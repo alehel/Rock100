9a6edd5ab9f696f43ee2e42c1ab1562855d22768
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2019-05-30 10:30:38 +0200

    Use single component to determine correct streaming service.

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import WelcomePage from './components/WelcomePage/';
-import Spotify from './components/MusicServiceHandler/Spotify/SpotifyTokenHandler'
 import Error404Page from './components/Error404Page';
+import CallbackHandler from "./components/CallbackHandler";
 
 const AppRoutes = () => (
     <Router>
         <Switch>
             <Route path="/" component={WelcomePage} exact />
-            <Route path="/spotify" component={Spotify} exact />
+            <Route path="/callback" component={CallbackHandler} />
             <Route component={Error404Page} />
         </Switch>
     </Router>
