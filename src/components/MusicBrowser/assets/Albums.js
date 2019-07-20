f8cd5a8f6e614e28e4a9414f79c751bd64d1a48f
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2019-07-20 18:22:00 +0200

    Add album 89 - 50.

--- src/components/MusicBrowser/assets/Albums.js
+++ src/components/MusicBrowser/assets/Albums.js
@@ -7,8 +7,8 @@ const albumList = [
         "listPosition": 100
     },
     {
-        "artist": "Tom Petty and the Heartbreakers",
-        "album": "Tom Petty and the Heartbreakers",
+        "artist": "Tom Petty And The Heartbreakers",
+        "album": "Tom Petty And The Heartbreakers",
         "year": 1976,
         "uri": "spotify:album:6TLTd0P2CUI0Q29AQ1LyFi",
         "listPosition": 99
@@ -22,7 +22,7 @@ const albumList = [
     },
     {
         "artist": "Pink Floyd",
-        "album": "The Piper at the Gates of Dawn",
+        "album": "The Piper At The Gates Of Dawn",
         "year": 1967,
         "uri": "spotify:album:0Fke5eiQ6lszQHlwiFygqn",
         "listPosition": 97
@@ -36,7 +36,7 @@ const albumList = [
     },
     {
         "artist": "Genesis",
-        "album": "The Lamb Lies Down on Broadway",
+        "album": "The Lamb Lies Down On Broadway",
         "year": 1974,
         "uri": "spotify:album:6kuKRIu6YGYxQLwasTL3Gx",
         "listPosition": 95
@@ -75,6 +75,286 @@ const albumList = [
         "year": 1991,
         "uri": "spotify:album:30Perjew8HyGkdSmqguYyg",
         "listPosition": 90
+    },
+    {
+        "artist": "Kiss",
+        "album": "Destroyer",
+        "year": 1976,
+        "uri": "",
+        "listPosition": 89
+    },
+    {
+        "artist": "David Bowie",
+        "album": "Hunky Dory",
+        "year": 1971,
+        "uri": "",
+        "listPosition": 88
+    },
+    {
+        "artist": "Yes",
+        "album": "Fragile",
+        "year": 1971,
+        "uri": "",
+        "listPosition": 87
+    },
+    {
+        "artist": "Bob Dylan",
+        "album": "Highway 61 Revisited",
+        "year": 1965,
+        "uri": "",
+        "listPosition": 86
+    },
+    {
+        "artist": "Queensrÿche",
+        "album": "Operation: Mindcrime",
+        "year": 1988,
+        "uri": "",
+        "listPosition": 85
+    },
+    {
+        "artist": "Rage Against The Machine",
+        "album": "Rage Against The Machine",
+        "year": 1992,
+        "uri": "",
+        "listPosition": 84
+    },
+    {
+        "artist": "Whitesnake",
+        "album": "1987",
+        "year": 1987,
+        "uri": "",
+        "listPosition": 83,
+    },
+    {
+        "artist": "Deep Purple",
+        "album": "Burn",
+        "year": 1974,
+        "uri": "",
+        "listPosition": 82,
+    },
+    {
+        "artist": "Ramones",
+        "album": "Ramones",
+        "year": 1976,
+        "uri": "",
+        "listPosition": 81
+    },
+    {
+        "artist": "Alice Cooper",
+        "album": "Billion Dollar Babies",
+        "year": 1973,
+        "uri": "",
+        "listPosition": 80
+    },
+    {
+        "artist": "Radiohead",
+        "album": "OK Computer",
+        "year": 1997,
+        "uri": "",
+        "listPosition": 79
+    },
+    {
+        "artist": "Iron Maiden",
+        "album": "Seventh Son Of A Seventh Son",
+        "year": 1988,
+        "uri": "",
+        "listPosition": 78
+    },
+    {
+        "artist": "The Who",
+        "album": "Tommy",
+        "year": 1969,
+        "uri": "",
+        "listPosition": 77
+    },
+    {
+        "artist": "Bon Jovi",
+        "album": "Slippery When Wet",
+        "year": 1986,
+        "uri": "",
+        "listPosition": 76
+    },
+    {
+        "artist": "Dire Straits",
+        "album": "Brothers In Arms",
+        "year": 1985,
+        "uri": "",
+        "listPosition": 75
+    },
+    {
+        "artist": "AC/DC",
+        "album": "Powerage",
+        "year": 1978,
+        "uri": "",
+        "listPosition": 74
+    },
+    {
+        "artist": "Bruce Springsteen",
+        "album": "Born In The USA",
+        "year": 1984,
+        "uri": "",
+        "listPosition": 73
+    },
+    {
+        "artist": "Cream",
+        "album": "Disraeli Gears",
+        "year": 1967,
+        "uri": "",
+        "listPosition": 72
+    },
+    {
+        "artist": "Kiss",
+        "album": "Alive!",
+        "year": 1975,
+        "uri": "",
+        "listPosition": 71
+    },
+    {
+        "artist": "UFO",
+        "album": "Strangers In The Night",
+        "year": 1979,
+        "uri": "",
+        "listPosition": 70
+    },
+    {
+        "artist": "Jimi Hendrix Experience",
+        "album": "Axis: Bold As Love",
+        "year": 1967,
+        "uri": "",
+        "listPosition": 69
+    },
+    {
+        "artist": "Lynyrd Skynyrd",
+        "album": "(pronounced 'leh-'nérd 'skin-'nérd)",
+        "year": 1973,
+        "uri": "",
+        "listPosition": 68
+    },
+    {
+        "artist": "The Who",
+        "album": "Live At Leeds",
+        "year": 1970,
+        "uri": "",
+        "listPosition": 67
+    },
+    {
+        "artist": "Bad Company",
+        "album": "Bad Company",
+        "year": 1974,
+        "uri": "",
+        "listPosition": 66
+    },
+    {
+        "artist": "Thin Lizzy",
+        "album": "Live And Dangerous",
+        "year": 1978,
+        "uri": "",
+        "listPosition": 65
+    },
+    {
+        "artist": "Derek & The Dominos",
+        "album": "Layla And Other Assorted Love Songs",
+        "year": 1970,
+        "uri": "",
+        "listPosition": 64
+    },
+    {
+        "artist": "Motörhead",
+        "album": "Ace Of Spades",
+        "year": 1980,
+        "uri": "",
+        "listPosition": 63
+    },
+    {
+        "artist": "Deep Purple",
+        "album": "In Rock",
+        "year": 1970,
+        "uri": "",
+        "listPosition": 62
+    },
+    {
+        "artist": "AC/DC",
+        "album": "Let There Be Rock",
+        "year": 1977,
+        "uri": "",
+        "listPosition": 61
+    },
+    {
+        "artist": "Def Leppard",
+        "album": "Pyromania",
+        "year": 1983,
+        "uri": "",
+        "listPosition": 60
+    },
+    {
+        "artist": "King Crimson",
+        "album": "In The Court Of The Crimson King",
+        "year": 1969,
+        "uri": "",
+        "listPosition": 59
+    },
+    {
+        "artist": "The Who",
+        "album": "Quadrophenia",
+        "year": 1973,
+        "uri": "",
+        "listPosition": 58
+    },
+    {
+        "artist": "Aerosmith",
+        "album": "Rocks",
+        "year": 1976,
+        "uri": "",
+        "listPosition": 57
+    },
+    {
+        "artist": "Jethro Tull",
+        "album": "Aqualung",
+        "year": 1971,
+        "uri": "",
+        "listPositions": 56
+    },
+    {
+        "artist": "Thin Lizzy",
+        "album": "Jailbreak",
+        "year": 1976,
+        "uri": "",
+        "listPositions": 55
+    },
+    {
+        "artist": "Judas Priest",
+        "album": "British Steel",
+        "year": 1980,
+        "uri": "",
+        "listPosition": 54
+    },
+    {
+        "artist": "The Beach Boys",
+        "album": "Pet Sounds",
+        "year": 1966,
+        "uri": "",
+        "listPosition": 53
+    },
+    {
+        "artist": "Bruce Springsteen",
+        "album": "Born To Run",
+        "year": 1975,
+        "uri": "",
+        "listPosition": 52
+    },
+    {
+        "artist": "Sex Pistols",
+        "album": "Never Mind The Bollocks, Here's The Sex Pistols",
+        "year": 1977,
+        "uri": "",
+        "listPosition": 51,
+    },
+    {
+        "artist": "Prince",
+        "album": "Purple Rain",
+        "year": 1984,
+        "uri": "",
+        "listPosition": 50
     }
 ]
 
