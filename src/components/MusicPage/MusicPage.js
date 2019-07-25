c7006eea082e209e5056198d89c3580dd7fedfab
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2019-07-25 16:06:06 +0200

    Move MusicPage styling to modularized SASS.

--- src/components/MusicPage/MusicPage.js
+++ src/components/MusicPage/MusicPage.js
@@ -1,28 +1,18 @@
 import React, {Component} from 'react';
 import MusicPlayer from "../MusicPlayer";
 import MusicBrowser from "../MusicBrowser/MusicBrowser";
-
-const styles = {
-    container: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column',
-        maxHeight: '100vh',
-        maxWidth: '100vw'
-    },
-}
+import {container} from './MusicPage.module.scss';
 
 class MusicPage extends Component {
 
     render() {
         return (
-            <div style={styles.container}>
+            <div className={container}>
                 <MusicBrowser spotifyAPI = {this.props.spotifyAPI} />
                 <MusicPlayer {...this.props}/>
             </div>
         );
     }
 }
-
 
 export default MusicPage;
