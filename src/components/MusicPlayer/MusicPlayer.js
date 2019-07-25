7dacfe398f6805215fef34b7ff0c74332659bffe
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2019-07-25 20:13:12 +0200

    Convert MusicPlayer to class component.

--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -1,18 +1,32 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import MusicBar from "./view/Bar";
 import FullScreen from './view/FullScreen';
 import PropTypes from "prop-types";
 
-const MusicPlayer = (props) => {
-    const [fullScreen, setFullScreen] = useState(false);
+class MusicPlayer extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <>
-            <FullScreen {...props} display={fullScreen} closeFullScreen={() => setFullScreen(false)} />
-            <MusicBar {...props} display={!fullScreen} openFullScreen={() => setFullScreen(true)}/>
-        </>
-    );
-};
+        this.state = {
+            fullScreen: false,
+        }
+
+        this.toggleFullScreen = this.toggleFullScreen.bind(this);
+    }
+
+    toggleFullScreen() {
+        const fullScreen = !this.state.fullScreen;
+        this.setState({fullScreen});
+    }
+
+    render() {
+        if(this.state.fullScreen) {
+            return <FullScreen {...this.props} display={this.state.fullScreen} closeFullScreen={this.toggleFullScreen}/>
+        } else {
+            return <MusicBar {...this.props} display={this.state.fullScreen} openFullScreen={this.toggleFullScreen}/>
+        }
+    }
+}
 
 MusicPlayer.propTypes = {
     artist: PropTypes.string.isRequired,
